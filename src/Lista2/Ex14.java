7d105167e32dea2cd4fd66ac856b87dba5e411ec
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-03-26 19:21:23 -0300

    adicionando Ex14

---
+++ src/Lista2/Ex14.java
@@ -0,0 +1,19 @@
+package Lista2;
+import java.util.Scanner;
+public class Ex14 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int n;
+		float H=0;
+		n = teclado.nextInt();
+		if(n<=0) {
+			System.out.println("Erro");
+		}
+		else {
+			for(int cont=2;cont<=n;cont++) {
+				H = H + 1.0f/cont;
+			}
+			System.out.printf("H = %.2f\n",H);
+		}
+	}
+}
