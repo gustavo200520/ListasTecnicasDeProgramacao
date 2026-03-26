37822f07d72d73c0f83055cdb0d8d1701c05c55c
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-03-26 18:52:13 -0300

    adicionando ex12

---
+++ src/Lista2/Ex12.java
@@ -0,0 +1,29 @@
+package Lista2;
+import java.util.Scanner;
+public class Ex12 {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		int x,n,base;
+		x = teclado.nextInt();
+		n = teclado.nextInt();
+		base = x;
+		if(n==0) {
+			System.out.println(1);
+		}
+		else if(n>0) {
+		
+			for(int cont=1;cont<n;cont++) {
+				x = x*base;
+			}
+			System.out.println(x);
+		}
+		else {
+			 float resultado = 1;
+	            for(int cont = 0; cont < -n; cont++) {
+	                resultado = resultado* base;
+	            }
+	            resultado = 1 / resultado;
+	            System.out.println(resultado);
+		}
+	}
+}
