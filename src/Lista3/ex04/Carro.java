a88509777900dc924458fdfb02fc0daee0fa2a2c
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-04-05 22:03:09 -0300

    adicionando Ex04 Lista 3

---
+++ src/Lista3/ex04/Carro.java
@@ -0,0 +1,28 @@
+package Lista3.ex04;
+
+public class Carro {
+	private String modelo;
+	private int ano;
+	private double valor;
+	
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public double getValor() {
+		return valor;
+	}
+}
---
+++ src/Lista3/ex04/Main.java
@@ -0,0 +1,33 @@
+package Lista3.ex04;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		ArrayList<Carro> listaCarro = new ArrayList<>();
+		
+		for(int cont=0;cont<4;cont++) {
+			Carro novoCarro = new Carro();
+
+			System.out.println("Insira o modelo: ");
+			novoCarro.setModelo(teclado.nextLine());
+			System.out.println("Insira o ano: ");
+			novoCarro.setAno(teclado.nextInt());
+			System.out.println("Insira o valor: ");
+			novoCarro.setValor(teclado.nextDouble());
+			teclado.nextLine();
+			listaCarro.add(novoCarro);
+		}
+		
+		for(Carro carroNovo : listaCarro) {
+			if(carroNovo.getAno() >=2020) {
+				System.out.printf("Modelo: %s\n",carroNovo.getModelo());
+				System.out.printf("Ano: %d\n",carroNovo.getAno());
+				System.out.printf("Valor: %.2f\n",carroNovo.getValor());
+				System.out.println("-----------------");
+			}
+		}
+	}
+
+}
