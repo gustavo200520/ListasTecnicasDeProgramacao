b9ddbf3a51e8a08fd5795f38898851a05dc229e4
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-04-12 16:15:21 -0300

    melhorando ex01 lista4

---
+++ src/Lista4/ex01/Carrinho.java
@@ -0,0 +1,49 @@
+package Lista4.ex01;
+import java.util.Scanner;
+import java.util.ArrayList;
+public class Carrinho {
+	
+	public double total=0, valor;
+	public ArrayList<Itens> listaItens = new ArrayList<>();
+
+	
+	public  void adicionarIten(Scanner teclado) {
+		String nome,codigo;
+		teclado.nextLine();
+		System.out.println("Insira o nome do produto: ");
+		nome=teclado.nextLine();
+		System.out.println("Insira o codigo do produto: ");
+		codigo=teclado.nextLine();
+		System.out.println("Insira o valor do produto: ");
+		valor=teclado.nextDouble();
+		total=valor+total;
+		Itens novoIten = new Itens(nome,codigo,valor);
+		System.out.println("Iten adicionado com sucesso\n");
+		listaItens.add(novoIten);
+	}
+	//------------------------------------------------
+	public  void removerItens(Scanner teclado) {
+		String remover;
+		teclado.nextLine();
+		System.out.println("Insira o codigo ou nome do produto que voce deseja remover");
+		remover = teclado.nextLine();
+		for(int cont=0;cont<listaItens.size();cont++) {
+			if(remover.equals(listaItens.get(cont).getNome()) || remover.equals(listaItens.get(cont).getCodigo())) {
+				total=total-listaItens.get(cont).getValor();
+				listaItens.remove(cont);
+				System.out.println("Iten removido com sucesso\n");
+			}
+		}
+	}
+	//------------------------------------------------
+	public  void listarItens(Scanner teclado) {
+		for(Itens itenNovo : listaItens) {
+			System.out.println("nome: "+itenNovo.getNome());
+			System.out.println("codigo: "+itenNovo.getCodigo());
+			System.out.println("valor: "+itenNovo.getValor());
+			System.out.println("-----------------------");
+		}
+		System.out.println("Valor total da compra: "+total+"\n");
+
+	}
+}
--- src/Lista4/ex01/Main.java
+++ src/Lista4/ex01/Main.java
@@ -2,29 +2,28 @@ package Lista4.ex01;
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-	public static ArrayList<Itens> listaItens = new ArrayList<>();
-	public static double total=0, valor;
+	
 	public static void main(String[] args) {
 		
 		Scanner teclado = new Scanner(System.in);
 		int opcoes;
-		
+		Carrinho meuCarrinho = new Carrinho();
 		do {
 			System.out.println("1-Adicionar iten\n2-Remover iten\n3-Ver lista\n4-Sair");
 			opcoes = teclado.nextInt();
 		
 			switch(opcoes) {
 				case 1:
-					adicionarIten(teclado);
+					meuCarrinho.adicionarIten(teclado);
 					break;
 				case 2:
-					removerItens(teclado);
+					meuCarrinho.removerItens(teclado);
 					break;
 				case 3:
-					listarItens(teclado);
+					meuCarrinho.listarItens(teclado);
 					break;
 				case 4:
-					listarItens(teclado);
+					meuCarrinho.listarItens(teclado);
 					System.out.println("Saindo...");
 					break;
 				default:
@@ -32,44 +31,5 @@ public class Main {
 			}
 		}while(opcoes!=4);
 	}
-	//------------------------------------------------
-	public static void adicionarIten(Scanner teclado) {
-		String nome,codigo;
-		teclado.nextLine();
-		System.out.println("Insira o nome do produto: ");
-		nome=teclado.nextLine();
-		System.out.println("Insira o codigo do produto: ");
-		codigo=teclado.nextLine();
-		System.out.println("Insira o valor do produto: ");
-		valor=teclado.nextDouble();
-		total=valor+total;
-		Itens novoIten = new Itens(nome,codigo,valor);
-		System.out.println("Iten adicionado com sucesso\n");
-		listaItens.add(novoIten);
-	}
-	//------------------------------------------------
-	public static void removerItens(Scanner teclado) {
-		String remover;
-		teclado.nextLine();
-		System.out.println("Insira o codigo ou nome do produto que voce deseja remover");
-		remover = teclado.nextLine();
-		for(int cont=0;cont<listaItens.size();cont++) {
-			if(remover.equals(listaItens.get(cont).getNome()) || remover.equals(listaItens.get(cont).getCodigo())) {
-				total=total-listaItens.get(cont).getValor();
-				listaItens.remove(cont);
-				System.out.println("Iten removido com sucesso\n");
-			}
-		}
-	}
-	//------------------------------------------------
-	public static void listarItens(Scanner teclado) {
-		for(Itens itenNovo : listaItens) {
-			System.out.println("nome: "+itenNovo.getNome());
-			System.out.println("codigo: "+itenNovo.getCodigo());
-			System.out.println("valor: "+itenNovo.getValor());
-			System.out.println("-----------------------");
-		}
-		System.out.println("Valor total da compra: "+total+"\n");
-
-	}
+	
 }
